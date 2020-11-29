3d1cad59e1d4af5fa487a2570bc2aeb86048bc62
Wojciech Buńka <wojtek.bunka@gmail.com>
2020-11-29 10:41:57 +0100

    Add basic homework

---
+++ Models/ToDoModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeInterpreter.Models
+{
+    public class ToDoModels
+    {
+        public string Title { get; set; }
+        public bool Isdone { get; set; }
+    }
+}
